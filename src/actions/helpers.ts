4cc43f68d601c7fc3aa57796a42bdb1a9fa9de57
Aimad MAJDOU <aimad.majdou@gmail.com>
2024-09-22 01:40:34 +0200

    Fix toaster and imrove error message

--- src/actions/helpers.ts
+++ src/actions/helpers.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import * as z from "zod";
 
 /**
@@ -11,6 +12,34 @@ import * as z from "zod";
  */
 export function isZodError(error: unknown): error is z.ZodError {
   return (error as z.ZodError).issues !== undefined;
+}
+
+/**
+ * isPrismaError
+ * Checks if the error is a Prisma known request error.
+ */
+export function isPrismaKnownError(
+  error: unknown
+): error is Prisma.PrismaClientKnownRequestError {
+  return (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    typeof error.code === "string"
+  );
+}
+
+export function isPrismaOtherError(
+  error: unknown
+): error is
+  | Prisma.PrismaClientUnknownRequestError
+  | Prisma.PrismaClientRustPanicError
+  | Prisma.PrismaClientInitializationError
+  | Prisma.PrismaClientValidationError {
+  return (
+    error instanceof Prisma.PrismaClientUnknownRequestError ||
+    error instanceof Prisma.PrismaClientRustPanicError ||
+    error instanceof Prisma.PrismaClientInitializationError ||
+    error instanceof Prisma.PrismaClientValidationError
+  );
 }
 
 /**
@@ -28,12 +57,38 @@ export function buildErrorMessage(
   error: unknown,
   fallbackErrorMessage: string
 ): string {
-  if (!isZodError(error)) {
-    return fallbackErrorMessage;
+  console.log(error);
+
+  if (isPrismaKnownError(error)) {
+    // Handle Prisma specific errors
+    if (error.meta) {
+      return Object.entries(error.meta).reduce((acc, [key, value]) => {
+        return acc + `${key}: ${value}\n`;
+      }, `Prisma Error Code: ${error.code}\n`);
+    }
+    return `Prisma Error Code: ${error.code}\nNo meta information available.`;
   }
 
-  // Combine all Zod issue messages into a single string
-  return error.issues.reduce((acc, issue) => {
-    return acc + issue.message + "\n";
-  }, "");
+  if (isPrismaOtherError(error)) {
+    // Handle Prisma other errors
+    if (error.message) {
+      return `Prisma Error: ${error.message}`;
+    }
+  }
+
+  if (isZodError(error)) {
+    // Combine all Zod issue messages into a single string
+    return error.issues.reduce((acc, issue) => {
+      return `${acc}${issue.message}\n`;
+    }, "Validtion Failed:\n");
+  }
+
+  if (error instanceof Error && error.message) {
+    return error.message;
+  }
+  if (typeof error === "string" && error.length > 0) {
+    return error;
+  }
+
+  return fallbackErrorMessage;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
@@ -26,11 +27,12 @@ export default function RootLayout({
       <body
         className={cn(
           "tw-min-h-screen tw-bg-background tw-font-sans tw-antialiased tw-text-black",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <main className="tw-container tw-px-12">{children}</main>
         {modal}
+        <Toaster />
       </body>
     </html>
   );
