15dd54abdf99d35232e5df22867309cb74aa26bd
Aimad MAJDOU <aimad.majdou@gmail.com>
2024-09-21 11:41:03 +0200

    Add code editor component

--- src/app/snippets/[id]/edit/_components/form.tsx
+++ src/app/snippets/[id]/edit/_components/form.tsx
@@ -1,16 +1,26 @@
 'use client';
 
+import { CodeEditor } from '@/components/code-editor';
 import { Snippet } from '@prisma/client';
+import { useState } from 'react';
 
 interface SnippetEditForm {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditForm) {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = '') => {
+    setCode(value);
+  };
+
   return (
     <div>
-      Client component has snippet with id: {snippet.id} and title:
-      {snippet.title}
+      <CodeEditor
+        title={snippet.title}
+        defaultValue={snippet.code}
+        onChange={handleEditorChange}
+      />
     </div>
   );
 }
---
+++ src/components/code-editor.tsx
@@ -0,0 +1,30 @@
+import Editor, { EditorProps } from '@monaco-editor/react';
+
+interface CodeEditorProps extends EditorProps {
+  readonly?: boolean;
+  title: string;
+}
+
+export function CodeEditor({
+  theme = 'vs-dark',
+  height = '40vh',
+  language = 'typescript',
+  options: optionsProp,
+  ...rest
+}: CodeEditorProps) {
+  const options = {
+    minimap: {
+      enabled: false,
+    },
+    ...optionsProp,
+  };
+  return (
+    <Editor
+      height={height}
+      theme={theme}
+      language={language}
+      options={options}
+      {...rest}
+    />
+  );
+}
