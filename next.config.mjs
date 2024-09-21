a55a9f6becfef68110cab89acca2dd940ae2f15d
Aimad MAJDOU <aimad.majdou@gmail.com>
2024-09-21 20:03:10 +0200

    Tidy up build process

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["shiki"],
+  typescript: {
+    ignoreBuildErrors: true, // check this in github actions
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // check this in github actions
+  },
 };
 
 export default nextConfig;
